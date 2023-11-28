d7d1a38deccfca07aad8bd92bbae0abd94be45b2
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2023-11-28 18:22:04 -0500

    spellcheck

--- ProjectManagment/src/components/NoProjectSelelcted.jsx
+++ ProjectManagment/src/components/NoProjectSelelcted.jsx
@@ -5,11 +5,11 @@ export default function({onStartAddProject}){
         <div className="mt-24 text-center w-2/3">
             <img src={noProjects} alt='An Empty book' className="w-16 h-16 object-contain mx-auto"/>
             <h1 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h1>
-            <h3 className="text-stone-400 mb-4">Select a Ptoject or get started with  anew one</h3>
+            <h3 className="text-stone-400 mb-4">Select a Project or get started with a new one</h3>
             <p className="mt-8">
                 <button className="px-4 py-2 text-xs md:text-base rounded-md bg-green-700 text-white  hover:text-red-600"
                 onClick={onStartAddProject}>
-                    Create New Ptoject
+                    Create New Project
                 </button>
 
             </p>
