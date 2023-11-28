f7fcf395a11f0b22f769b03777fa0dfa7e066b74
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2023-11-28 13:30:47 -0500

    Added NoProjectSelected Coponennt

--- ProjectManagment/src/App.jsx
+++ ProjectManagment/src/App.jsx
@@ -1,4 +1,5 @@
 import AddProject from "./components/AddProject";
+import NoProjectSelelcted from "./components/NoProjectSelelcted";
 import Sidebar from "./components/Sidebar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
       <AddProject />
+      <NoProjectSelelcted/>
     </main>
 
   );
---
+++ ProjectManagment/src/components/NoProjectSelelcted.jsx
@@ -0,0 +1,18 @@
+import noProjects from '../assets/noProjects.png';
+
+export default function(){
+    return (
+        <div className="mt-24 text-center w-2/3">
+            <img src={noProjects} alt='An Empty book' className="w-16 h-16 object-contain mx-auto"/>
+            <h1 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h1>
+            <h3 className="text-stone-400 mb-4">Select a Ptoject or get started with  anew one</h3>
+            <p className="mt-8">
+                <button className="px-4 py-2 text-xs md:text-base rounded-md bg-green-700 text-white  hover:text-red-600">
+                    Create New Ptoject
+                </button>
+
+            </p>
+        </div>
+    )
+
+}
